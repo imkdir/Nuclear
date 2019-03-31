31ffc9afb99bd4e5e7fa527ce40f66bb539a1690
Tung CHENG <hichengd@gmail.com>
2019-03-31 10:59:05 +0800

    Initial Commit

---
+++ NDBKit/NDBKit.h
@@ -0,0 +1,19 @@
+//
+//  NDBKit.h
+//  NDBKit
+//
+//  Created by Tung CHENG on 2019/3/31.
+//  Copyright © 2019 Tung CHENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NDBKit.
+FOUNDATION_EXPORT double NDBKitVersionNumber;
+
+//! Project version string for NDBKit.
+FOUNDATION_EXPORT const unsigned char NDBKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NDBKit/PublicHeader.h>
+
+
